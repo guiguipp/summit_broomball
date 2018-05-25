31756aefbe396cbe8ceace0551421de9d6a2f947
Guillaume Paugam <guipaugam@icloud.com>
2018-05-25 18:42:59 -0500

    Consolided filtering on Autodraft function + added info to easy testing

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,15 +2,23 @@ const players = require("./players_db")
 let team1Name = "Dark Team";
 let team2Name = "Light Team"
 // retrieve data: file required + . + var name + . + element of the object
+// These examples are easy, because their name is a number (shows how the draft works)
+let easyEx =        [players.player1, players.player2, players.player3, players.player4, players.player5, players.player6, players.player7, players.player8, players.player9, players.player10, players.player11, players.player12, players.player13, players.player14, players.player15, players.player16, players.player17, players.player18, players.player19, players.player20, players.player21, players.player22, players.player23, players.player24, players.player25, players.player26, players.player27, players.player28, players.player29, players.player30]
+let easyExRandom =  [players.player1, players.player12, players.player29, players.player3, players.player5, players.player14, players.player8, players.player9, players.player10, players.player23, players.player11, players.player13, players.player15, players.player16, players.player17, players.player18, players.player19, players.player20, players.player22, players.player21, players.player4, players.player7, players.player6, players.player24, players.player25, players.player26, players.player28, players.player30, players.player27, players.player2]
 // Examples:
 // console.log("Player1", players.player1);
 // console.log("Player1", players.player1.shortname);
-var availablePlayers =  [players.player1, players.player2, players.player3, players.player4, players.player5, players.player6, players.player7, players.player8, players.player9, players.player10, players.player11, players.player12, players.player13, players.player14, players.player15, players.player16, players.player17, players.player18, players.player19, players.player20, players.player21, players.player22, players.player23, players.player24, players.player25, players.player26, players.player27, players.player28, players.player29, players.player30]
-var randomLevels =      [players.player1, players.player12, players.player29, players.player3, players.player5, players.player14, players.player8, players.player9, players.player10, players.player23, players.player11, players.player13, players.player15, players.player16, players.player17, players.player18, players.player19, players.player20, players.player22, players.player21, players.player4, players.player7, players.player6, players.player24, players.player25, players.player26, players.player28, players.player30, players.player27, players.player2]
+
+let availablePlayers = [players.barry, players.becca, players.ben, players.carity, players.chad, players.dane, players.foss, players.graham, players.grant, players.gui, players.jamie, players.jason, players.jen, players.joe, players.karissa, players.kiemo, players.kim, players.matt, players.moore, players.paul, players.schawnski, players.shalanah, players.sheila, players.stan, players.steve, players.sui, players.todd, players.tony, players.vilks, players.weddie];
+
 // Bear in mind that this is how we will need to create our arrays: the team name, and then their picks (in order)
-var picksTeam1 = {name: team1Name, entered: "time", picks: [players.player1, players.player2, players.player3, players.player4, players.player5, players.player6, players.player7, players.player8, players.player9, players.player10, players.player11, players.player12, players.player13, players.player14, players.player15, players.player16, players.player17, players.player18, players.player19, players.player20, players.player21, players.player22, players.player23, players.player24, players.player25, players.player26, players.player27, players.player28,players.player29,players.player30]}
-let picksTeam2 = {name : team2Name, entered: "time", picks: [players.player1, players.player2, players.player3, players.player4, players.player5, players.player6, players.player7, players.player8, players.player9, players.player10, players.player11, players.player12, players.player13, players.player14, players.player15, players.player16, players.player17, players.player18, players.player19, players.player20, players.player21, players.player22, players.player23, players.player24, players.player25, players.player26, players.player27, players.player28,players.player29,players.player30]}
-// let picksTeam3 = {name : team2Name, picks: [players.player30, players.player29, players.player28, players.player27, players.player26, players.player25, players.player24, players.player23, players.player22, players.player21, players.player20, players.player19, players.player18, players.player17, players.player16, players.player15, players.player14, players.player13, players.player12, players.player11, players.player10, players.player9, players.player8, players.player7, players.player6, players.player5, players.player4, players.player3,players.player2,players.player1]}
+let easyExPicksTeam1 = {name: team1Name, entered: "time", picks: [players.player1, players.player2, players.player3, players.player4, players.player5, players.player6, players.player7, players.player8, players.player9, players.player10, players.player11, players.player12, players.player13, players.player14, players.player15, players.player16, players.player17, players.player18, players.player19, players.player20, players.player21, players.player22, players.player23, players.player24, players.player25, players.player26, players.player27, players.player28,players.player29,players.player30]}
+let easyExPicksTeam2 = {name : team2Name, entered: "time", picks: [players.player1, players.player2, players.player3, players.player4, players.player5, players.player6, players.player7, players.player8, players.player9, players.player10, players.player11, players.player12, players.player13, players.player14, players.player15, players.player16, players.player17, players.player18, players.player19, players.player20, players.player21, players.player22, players.player23, players.player24, players.player25, players.player26, players.player27, players.player28,players.player29,players.player30]}
+let easyExPicksTeam3 = {name : team2Name, picks: [players.player30, players.player29, players.player28, players.player27, players.player26, players.player25, players.player24, players.player23, players.player22, players.player21, players.player20, players.player19, players.player18, players.player17, players.player16, players.player15, players.player14, players.player13, players.player12, players.player11, players.player10, players.player9, players.player8, players.player7, players.player6, players.player5, players.player4, players.player3,players.player2,players.player1]}
+// * real life * examples
+let exPicksTeam1 = {name: team1Name, entered: "time", picks: [players.matt, players.vilks, players.carity, players.foss, players.grant, players.karissa, players.kiemo, players.weddie, players.barry, players.gui, players.joe, players.paul, players.shalanah, players.steve, players.becca, players.chad, players.dane, players.graham, players.schawnski, players.sheila, players.todd, players.tony, players.ben, players.moore, players.jason, players.sui, players.jamie, players.jen, players.kim, players.stan]};
+let exPicksTeam2 = {name: team2Name, entered: "time", picks: [players.matt, players.vilks, players.carity, players.foss, players.grant, players.karissa, players.kiemo, players.weddie, players.barry, players.gui, players.joe, players.paul, players.shalanah, players.steve, players.becca, players.chad, players.dane, players.graham, players.schawnski, players.sheila, players.todd, players.tony, players.ben, players.moore, players.jason, players.sui, players.jamie, players.jen, players.kim, players.stan]};
+let exPicksTeam3 = {name: team2Name, entered: "time", picks: [players.barry, players.becca, players.ben, players.carity, players.chad, players.dane, players.foss, players.graham, players.grant, players.gui, players.jamie, players.jason, players.jen, players.joe, players.karissa, players.kiemo, players.kim, players.matt, players.moore, players.paul, players.schawnski, players.shalanah, players.sheila, players.stan, players.steve, players.sui, players.todd, players.tony, players.vilks, players.weddie]};
 
 // utility function to get a player from a "pick" array, and push it to the roster array
 const pick = (index,inputArray, outputArray) => {
@@ -154,7 +162,7 @@ const filterTeams = (arrayOfPlayerObjects) => {
     console.log("************\n");
     // display the shortname of the player objects for the whole team
     rosterTeam1.forEach((e) => {
-        console.log(e.shortname);
+        console.log(`${e.shortname} (${e.level})`);
     })
     // filter player objects according to name of the team #2
     rosterTeam2 = arrayOfPlayerObjects.filter((e) => e.team === team2Name)
@@ -163,7 +171,7 @@ const filterTeams = (arrayOfPlayerObjects) => {
     console.log("************\n");
     // display the shortname of the player objects for the whole team
     rosterTeam2.forEach((e) => {
-        console.log(e.shortname);
+        console.log(`${e.shortname} (${e.level})`);
     })
 
 }
@@ -176,7 +184,7 @@ const autoDraft = (arrayOfAvailablePlayers) => {
     let output = arrayOfAvailablePlayers.reduce((levels,player) => {
         levels[player.level] = levels[player.level] || [];
         levels[player.level].push({
-            name: player.shortname,
+            shortname: player.shortname,
             position: player.position,
             level: player.level
         });
@@ -191,29 +199,45 @@ const autoDraft = (arrayOfAvailablePlayers) => {
         let playersArray = playersByLevel[1]
         // console.log("players array non randomized: ", playersArray)
         randomize(playersArray, mixedRosters)
-    }
+        }
     for (i = 0; i < mixedRosters.length; i++) {
-        // pushing every other player in respective teams
+        // assigning different team to every other player
         if (i%2 === 0) {
-            rosterTeam1.push(mixedRosters[i])
-        }
+            mixedRosters[i].team = team1Name;
+            }
         else {
-            rosterTeam2.push(mixedRosters[i])
+            mixedRosters[i].team = team2Name;
+            }
         }
-    }
-    console.log("****************");
-    console.log("Roster Team1: \n", rosterTeam1);
-    console.log("****************");
-    console.log("Roster Team2: \n", rosterTeam2);
+    filterTeams(mixedRosters);
 }
 
-// Call the serpentine draft function
-// serpentineDraft(picksTeam1,picksTeam2)
+// CHECK: CALL THE SERPENTINE DRAFT FUNCTION:
+// ******************************************
+// ******************************************
+// serpentineDraft(easyExPicksTeam1,easyExPicksTeam2) // worst case scenario (exact same picks)
+// serpentineDraft(easyExPicksTeam1,easyExPicksTeam3)
+// serpentineDraft(exPicksTeam1,exPicksTeam2) // worst case scenario (exact same picks)
+// serpentineDraft(exPicksTeam1,exPicksTeam3)
 
-// Call the alternate draft function
-// alternateDraft(picksTeam1,picksTeam2)
 
-// Call the randomize draft function
+// CHECK: CALL THE ALTERNATE DRAFT FUNCTION:
+// ******************************************
+// ******************************************
+// alternateDraft(easyExPicksTeam1,easyExPicksTeam2) // worst case scenario (exact same picks)
+// alternateDraft(easyExPicksTeam1,easyExPicksTeam3)
+// alternateDraft(exPicksTeam1,exPicksTeam2) // worst case scenario (exact same picks)
+// alternateDraft(exPicksTeam1,exPicksTeam3)
+
+// CHECK: CALL THE AUTO DRAFT FUNCTION
+// ******************************************
+// ******************************************
+// autoDraft(easyEx)
 // autoDraft(availablePlayers)
+
+/* 
+=> do not draft the goalies (can end up in the same team, duh...)
+=> double check that pick is available, before drafting
+*/
 
 module.exports = serpentineDraft;
