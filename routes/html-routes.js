8d69cd809888a30fa998bc675fc318e6d8e9ba38
Pamela Pelkola <pelkola.pam@gmail.com>
2018-06-02 12:14:42 -0500

    updated routes for html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -27,7 +27,3 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../views/player.html"));
   });
 
-  app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-};
