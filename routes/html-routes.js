b3013de6ab62abcebf5e15d2419f1db017a998d6
Mustafa Farah <mustafa.farah@outlook.com>
2018-06-02 13:46:15 -0500

    RouteFix_mustafa

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,35 +2,34 @@ var path = require("path");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../views/index.html"));
   });
 
-  app.get("/index", function(req, res) {
+  app.get("/index", function (req, res) {
     res.sendFile(path.join(__dirname, "../views/index.html"));
   });
 
-  app.get("/league", function(req, res) {
+  app.get("/league", function (req, res) {
     res.sendFile(path.join(__dirname, "../views/league.html"));
   });
 
-  app.get("/draft", function(req, res) {
+  app.get("/draft", function (req, res) {
     res.sendFile(path.join(__dirname, "../views/draft.html"));
   });
 
-  app.get("/player", function(req, res) {
+  app.get("/player", function (req, res) {
     res.sendFile(path.join(__dirname, "../views/player.html"));
   });
 
 
-  app.get("/contact", function(req, res) {
+  app.get("/contact", function (req, res) {
     res.sendFile(path.join(__dirname, "../views/contact.html"));
   });
 
 };
-
--- routes/roster-api-routes.js
+++ routes/roster-api-routes.js
@@ -1,64 +1,69 @@
 var path = require("path")
 var db = require("../models");
 
-module.exports = function(app) {
-  app.get("/api/rosters", function(req, res) {
-    db.Roster.findAll({}).then(function(dbRoster) {
+module.exports = function (app) {
+  app.get("/api/rosters", function (req, res) {
+    db.Roster.findAll({}).then(function (dbRoster) {
       res.json(dbRoster);
-      });
     });
-  app.get("/api/rosters/:id", function(req, res) {
-    let id = parseInt(req.params.id); 
-    db.Roster.findById(id).then(function(dbRoster) {
+  });
+  app.get("/api/rosters/:id", function (req, res) {
+    let id = parseInt(req.params.id);
+    db.Roster.findById(id).then(function (dbRoster) {
       res.json(dbRoster);
-      });  
     });
-  app.get("/api/rosters/game/:game_id", function(req, res) {
+  });
+  app.get("/api/rosters/game/:game_id", function (req, res) {
     db.Roster.findAll({
-      where: 
-        {GameId: req.params.game_id, availability: true}
-    }).then(function(dbRoster) {
+      where: {
+        GameId: req.params.game_id,
+        availability: true
+      }
+    }).then(function (dbRoster) {
       res.json(dbRoster);
-      });  
     });
-  app.post("/api/rosters", function(req, res) {
+  });
+  app.post("/api/rosters", function (req, res) {
     db.Roster.create({
-      player: req.body.player, 
-      GameId: req.body.GameId,
-      availability: req.body.availability,
-      editable: true
+        player: req.body.player,
+        GameId: req.body.GameId,
+        availability: req.body.availability,
+        editable: true
       })
-      .then(function(dbRoster) {
+      .then(function (dbRoster) {
         res.json(dbRoster);
-        });
       });
-    
-     
-  app.put("/api/rosters/:id", function(req, res) {
-    db.Roster.update({
-      player: req.body.player, 
-      goals: req.body.goals,
-      assists: req.body.assists,
-      availability: req.body.availability,
-      captain1Pick: req.body.captain1Pick,
-      captain2Pick: req.body.captain2Pick,
-      team: req.body.team
-      },
-      {
-      returning: true,
-      where: {id: req.params.id}
-      })
-      .then(function(dbRoster) {
-        res.json(dbRoster);
-        });
-      });
-  app.delete("/api/rosters/:id", function(req, res) {
-    db.Roster.destroy({
-      where: {id: req.params.id}
-      })
-      .then(function(dbRoster) {
-      res.json(dbRoster);
-      });
-    });
+  });
 
-  };
+
+  app.put("/api/rosters/:id", function (req, res) {
+    db.Roster.update({
+        player: req.body.player,
+        goals: req.body.goals,
+        assists: req.body.assists,
+        availability: req.body.availability,
+        captain1Pick: req.body.captain1Pick,
+        captain2Pick: req.body.captain2Pick,
+        team: req.body.team
+      }, {
+        returning: true,
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function (dbRoster) {
+        res.json(dbRoster);
+      });
+  });
+  app.delete("/api/rosters/:id", function (req, res) {
+    db.Roster.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function (dbRoster) {
+        res.json(dbRoster);
+      });
+  });
+
+};
