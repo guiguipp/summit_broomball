9ed0de1286396091f28d76675b1acb16b5bf2bda
Guillaume Paugam <guipaugam@icloud.com>
2018-06-02 21:39:12 -0500

    Recovery mode

--- routes/roster-api-routes.js
+++ routes/roster-api-routes.js
@@ -1,79 +1,64 @@
-var path = require("path");
+var path = require("path")
 var db = require("../models");
-var Sequelize = require("sequelize");
 
-
-module.exports = function (app) {
-  app.get("/api/rosters", function (req, res) {
-    db.Roster.findAll({}).then(function (dbRoster) {
+module.exports = function(app) {
+  app.get("/api/rosters", function(req, res) {
+    db.Roster.findAll({}).then(function(dbRoster) {
       res.json(dbRoster);
+      });
     });
-  });
-  app.get("/api/rosters/:id", function (req, res) {
-    let id = parseInt(req.params.id);
-    db.Roster.findById(id).then(function (dbRoster) {
+  app.get("/api/rosters/:id", function(req, res) {
+    let id = parseInt(req.params.id); 
+    db.Roster.findById(id).then(function(dbRoster) {
       res.json(dbRoster);
+      });  
     });
-  });
-  app.get("/api/rosters/game/:game_id", function (req, res) {
+  app.get("/api/rosters/game/:game_id", function(req, res) {
     db.Roster.findAll({
-      where: {
-        GameId: req.params.game_id,
-        availability: true
-      }
-    }).then(function (dbRoster) {
+      where: 
+        {GameId: req.params.game_id, availability: true}
+    }).then(function(dbRoster) {
       res.json(dbRoster);
+      });  
     });
-  });
-  app.get("/api/rosters/game/:game_id/players", function(req, res) {
-    db.sequelize.query('SELECT DISTINCT shortname,Rosters.id,player_level AS level FROM rosters INNER JOIN players ON rosters.player = players.shortname WHERE GameId=? AND availability=true',
-    {replacements: [req.params.game_id], type: db.sequelize.QueryTypes.SELECT
-      }).then(function(dbRoster) {
+  app.post("/api/rosters", function(req, res) {
+    db.Roster.create({
+      player: req.body.player, 
+      GameId: req.body.GameId,
+      availability: req.body.availability,
+      editable: true
+      })
+      .then(function(dbRoster) {
         res.json(dbRoster);
         });
       });
-
-  app.post("/api/rosters", function(req, res) {
-    db.Roster.create({
-        player: req.body.player,
-        GameId: req.body.GameId,
-        availability: req.body.availability,
-        editable: true
-      })
-      .then(function (dbRoster) {
-        res.json(dbRoster);
-      });
-  });
-
     
+     
   app.put("/api/rosters/:id", function(req, res) {
     db.Roster.update({
-        player: req.body.player,
-        goals: req.body.goals,
-        assists: req.body.assists,
-        availability: req.body.availability,
-        captain1Pick: req.body.captain1Pick,
-        captain2Pick: req.body.captain2Pick,
-        team: req.body.team
-      }, {
-        returning: true,
-        where: {
-          id: req.params.id
-        }
+      player: req.body.player, 
+      goals: req.body.goals,
+      assists: req.body.assists,
+      availability: req.body.availability,
+      captain1Pick: req.body.captain1Pick,
+      captain2Pick: req.body.captain2Pick,
+      team: req.body.team
+      },
+      {
+      returning: true,
+      where: {id: req.params.id}
       })
-      .then(function (dbRoster) {
+      .then(function(dbRoster) {
         res.json(dbRoster);
+        });
       });
-  });
-  app.delete("/api/rosters/:id", function (req, res) {
+  app.delete("/api/rosters/:id", function(req, res) {
     db.Roster.destroy({
-        where: {
-          id: req.params.id
-        }
+      where: {id: req.params.id}
       })
-      .then(function (dbRoster) {
-        res.json(dbRoster);
+      .then(function(dbRoster) {
+      res.json(dbRoster);
       });
-  });
+    });
 
-};
+  };
