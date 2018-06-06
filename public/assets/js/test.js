b33333e1f1507748476e34c14474148d1a6e2de4
Guillaume Paugam <guipaugam@icloud.com>
2018-06-06 18:08:57 -0500

    Tried to update the score, and have the serpentine function working

--- public/assets/js/test.js
+++ public/assets/js/test.js
@@ -1,38 +1,62 @@
-let dataFromAPI= [
-{id: 63, player: "Carity", captain1Pick: 0, gameId: 3},
-{id: 64, player: "Foss", captain1Pick: 0, gameId: 3},
-{id: 65, player: "Grant", captain1Pick: 0, gameId: 3},
-{id: 67, player: "Kiemo", captain1Pick: 0, gameId: 3},
-{id: 69, player: "Vilks", captain1Pick: 0, gameId: 3},
-{id: 70, player: "Paul", captain1Pick: 0, gameId: 3},
-{id: 72, player: "Joe", captain1Pick: 0, gameId: 3},
-{id: 73, player: "Gui", captain1Pick: 0, gameId: 3},
-{id: 75, player: "Steve", captain1Pick: 0, gameId: 3},
-{id: 77, player: "Dane", captain1Pick: 0, gameId: 3},
-{id: 79, player: "Graham", captain1Pick: 0, gameId: 3},
-{id: 80, player: "Shawnski", captain1Pick: 0, gameId: 3},
-{id: 82, player: "Todd", captain1Pick: 0, gameId: 3},
-{id: 83, player: "Tony", captain1Pick: 0, gameId: 3},
-{id: 84, player: "Ben (G)", captain1Pick: 0, gameId: 3},
-{id: 85, player: "Moore (G)", captain1Pick: 0, gameId: 3},
-{id: 86, player: "Jason", captain1Pick: 0, gameId: 3},
-{id: 88, player: "Jamie", captain1Pick: 0, gameId: 3},
-{id: 91, player: "Stan", captain1Pick: 0, gameId: 3},
-{id: 68, player: "Weddie", captain1Pick: 1, gameId: 3},
-{id: 81, player: "Sheila", captain1Pick: 2, gameId: 3},
-{id: 74, player: "Shalanah", captain1Pick: 3, gameId: 3},
-{id: 76, player: "Becca", captain1Pick: 4, gameId: 3}]
+/*
+$("#serpentine_draft").click(function() {
+let gameId = $(this).attr("game_id");
+let gameDate = $(this).attr("game_date");
+let locked = $(this).attr("locked");
+let team1Array = [];
+let team2Array = [];
+let arrayOfAvailablePlayers = [];
+console.log("gameId: ", gameId,"gameDate: ", gameDate,"locked: ",locked)
+// we get the available players because their availability might have changed since the teams were picked
+console.log(gettingAllArrays(getAvailablePlayers(gameId))(getCapt1Ranked(team1Array,gameId))(getCapt2Ranked(team2Array,gameId)))
+})
+*/
+function getAvailablePlayers(idOfGame) {
+    $.ajax({ url: currentURL + "/api/rosters/game/" + idOfGame + "/availability/1/player/ASC", method: "GET" }).then(function(dataFromAPI) {
+        dataFromAPI.forEach((e) => {
+            arrayOfAvailablePlayers.push(e.player)
+            })
+        })
 
-let alreadyRankedPlayers = []
-    dataFromAPI.forEach((e,i) => {
-        if(e.captain1Pick > 0) {
-            arrayOfRankedPlayers.push(e)
-        }
-    });
-    let numOfRankedPlayers = arrayOfRankedPlayers.length
-    dataFromAPI.splice(dataFromAPI.length-arrayOfRankedPlayers.length, arrayOfRankedPlayers.length)
-    let rankedArray = arrayOfRankedPlayers.concat(dataFromAPI); 
+    }
 
-    
-    console.log("dataFromAPI: ", "dataFromAPI", "(length is third: ",dataFromAPI.length,")")
-    console.log("finally... if didn't push at last: ", betterArray)
+function getCapt1Ranked(array, idOfGame) {
+    $.ajax({ url: currentURL + "/api/rosters/" + idOfGame + "/ranked/captain1", method: "GET" }).then(function(dataFromAPI) {
+        dataFromAPI.forEach((e) => {
+            team1Array.push(e.player)
+            console.log("team1Array: ", array)
+            })
+        })
+    }
+
+function getCapt2Ranked(array, idOfGame) {
+    $.ajax({ url: currentURL + "/api/rosters/" + idOfGame + "/ranked/captain2", method: "GET" }).then(function(dataFromAPI) {
+        dataFromAPI.forEach((e) => {
+            array.push(e.player)
+            console.log("team2Array: ", array)
+            })
+        })
+    }
+// serpentineDraft(team1Array, team2Array, arrayOfAvailablePlayers))
+
+    // console.log(arrayOfAvailablePlayers)
+
+/*
+let gettingAllArrays = 
+    avail =>
+        capt1 =>
+            capt2 =>
+            avail
+            capt1
+            capt2
+            */
+let gettingAllArrays = 
+    avail =>
+        capt1=>
+            capt2=>
+                avail + "trying"
+                capt1 + "to understand"
+                capt2 + "this thing"
+
+
+console.log(gettingAllArrays("name")("size")("anything"))
