390e5e6cf9d695bf7156c34a3aa9c51d7a3889f6
@Donskoy <diman@rocketmail.com>
2018-06-10 14:57:50 -0500

    jawsdb config

--- config/config.js
+++ config/config.js
@@ -17,10 +17,7 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    dialect: 'mysql',
+    use_env_variable: "JAWSDB_URL",
+    dialect: 'mysql'
   }
 };
